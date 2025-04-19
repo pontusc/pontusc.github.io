9a711305133ce3d55ebbe91aa5b1739c4576ca35
pontusc <pontuscurtsson@gmail.com>
2025-04-19 13:08:47 +0200

    Init

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,45 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { Tabs } from "radix-ui";
+import { useState } from "react";
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [tab, setTab] = useState('tab1')
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
+    <div>
+      <div className="header">
+        <h1>
+          Pontus Curtsson
+        </h1>
+        <h3>
+          DevOps Engineer
+        </h3>
+        <p className='description'>
+          Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      <Tabs.Root value={tab} onValueChange={val => setTab(val)}>
+        <Tabs.List>
+          <Tabs.Trigger className={`tabButton ${tab === 'tab1' ? 'activeTab' : ''}`} value="tab1">
+            {tab === 'tab1' ? '>' : ''} Skills
+          </Tabs.Trigger>
+          <Tabs.Trigger className={`tabButton ${tab === 'tab2' ? 'activeTab' : ''}`} value="tab2">
+            {tab === 'tab2' ? '>' : ''} Projects
+          </Tabs.Trigger>
+          <Tabs.Trigger className={`tabButton ${tab === 'tab3' ? 'activeTab' : ''}`} value="tab3">
+            {tab === 'tab3' ? '>' : ''} Links
+          </Tabs.Trigger>
+        </Tabs.List>
+        <Tabs.Content className="TabsContent" value="tab1">
+          Skillscontent
+        </Tabs.Content>
+        <Tabs.Content className="TabsContent" value="tab2">
+          ynnaf
+        </Tabs.Content>
+        <Tabs.Content className="TabsContent" value="tab3">
+          nosstruc
+        </Tabs.Content>
+      </Tabs.Root >
+    </div >
   )
 }
 
